12490bfb134fe1551a5611f802f3279bd0fe54d3
ZJW-92 <zjwei0201@gmail.com>
2022-12-31 12:17:53 +0100

    add menuitem.jsx

---
+++ src/components/Menuitem/MenuItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{ color: '#e6c18f' }}>{title}</p>
+      </div>
+      <div className="app__menuitem-dash" />
+      <div className="app__menuitem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: 'silver' }}>{tags}</p>
+    </div>
+  </div>
+);
+
+export default MenuItem;
