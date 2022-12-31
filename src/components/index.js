808a7159ad7a89b05489c0d793c5a695f254d7f5
ZJW-92 <zjwei0201@gmail.com>
2022-12-31 12:17:11 +0100

    add components index.js

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import SubHeading from './SubHeading/SubHeading';
+
+import MenuItem from './Menuitem/MenuItem';
+import Navbar from './Navbar/Navbar';
+
+export {
+  SubHeading,
+  MenuItem,
+  Navbar,
+};
