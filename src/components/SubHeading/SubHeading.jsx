c104c555b0b8ab0c27235e375442ddc6671f8eab
ZJW-92 <zjwei0201@gmail.com>
2022-12-31 12:19:06 +0100

    add subheading.jsx

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '3rem' }}>
+    <p className="p__cormorant">{title}</p>
+  </div>
+);
+
+export default SubHeading;
