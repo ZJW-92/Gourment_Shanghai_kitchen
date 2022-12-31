3a302bc8a6ca2fa873e27185fc5919b7ee4d19e4
ZJW-92 <zjwei0201@gmail.com>
2022-12-31 12:14:35 +0100

    add constants images.js

---
+++ src/constants/images.js
@@ -0,0 +1,42 @@
+import bg from '../assets/bg.png';
+
+import meat from '../assets/meat.jpg'
+import xiaolongbao from '../assets/xiaolongbao.jpg';
+import welcome from '../assets/welcome.png';
+import findus from '../assets/findus.jpg';
+import chicken from '../assets/chicken.jpg'
+import duck from '../assets/duck.jpg'
+import xunyu from '../assets/xunyu.jpg'
+import sh from '../assets/sh.jpg';
+import noodle from '../assets/noodle.jpg';
+import fishnoodle from '../assets/fishnoodle.jpg'
+import guotie from '../assets/guotie.jpg';
+import kaofu from '../assets/kaofu.png'
+import yanduxian from '../assets/yanduxian.jpg';
+import eggdumpling from '../assets/eggdumpling.jpg'
+import beer from '../assets/beer.png'
+import beer2 from '../assets/beer2.jpg'
+import beer3 from '../assets/beer3.jpg'
+
+export default {
+  bg,
+  xiaolongbao,
+  meat,
+ 
+  welcome,
+  findus,
+  sh,
+  noodle,
+  fishnoodle,
+  guotie,
+  kaofu,
+  yanduxian,
+  eggdumpling,
+  beer,
+  beer2,
+  beer3,
+  xunyu,
+  chicken,
+  duck,
+
+};
