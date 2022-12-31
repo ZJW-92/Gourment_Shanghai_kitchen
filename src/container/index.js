6fa9a40a95568a604e5dc4e539b994bf43cb07a2
ZJW-92 <zjwei0201@gmail.com>
2022-12-31 14:10:19 +0100

    add index.js in container

---
+++ src/container/index.js
@@ -0,0 +1,22 @@
+import AboutUs from './AboutUs/AboutUs';
+
+import FindUs from './Findus/FindUs';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+
+
+import SpecialMenu from './Menu/SpecialMenu';
+
+
+export {
+  AboutUs,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+
+  SpecialMenu,
+ 
+
+};
