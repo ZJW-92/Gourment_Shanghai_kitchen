69602d1712d799e9e731128e4971c2ab363d4b85
ZJW-92 <zjwei0201@gmail.com>
2022-12-31 12:13:57 +0100

    add constants data.js

---
+++ src/constants/data.js
@@ -0,0 +1,161 @@
+import images from './images';
+
+
+const cold_menu =[
+  {
+    title: 'Kaofu gluten salad 四喜烤麸',
+    price: '99 kr ',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+  {
+    title: 'Egg dumplings 蛋饺',
+    price: '99 kr/ 12st ',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+  {
+    title: 'Shanghai Smoked fish 上海熏魚 ',
+    price: '179 kr',
+    tags: ' Recommend | 👍👍👍👍 ',
+  },
+
+  {
+    title: 'Chilled Crystal Ham 水晶肴肉',
+    price: '99 kr ',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+  {
+    title: 'braised duck with soy sauce 上海酱鸭',
+    price: '149kr / half',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+  {
+    title: 'Shanghai boiled chicken 上海白斩鸡',
+    price: '129kr / half',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+
+]
+
+
+
+const menu =[
+  
+  {
+    title: 'Spring rolls 春卷',
+    price: '99 kr/ 6st ',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+ 
+
+  {
+    title: 'Fried dumplings 锅贴',
+    price: '99 kr / 10st',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+  {
+    title: 'XiaoLongBao 小笼包',
+    price: '99 kr / 12st',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+
+  {
+    title: 'Shanghai Chow Mein 上海炒面',
+    price: '99 kr',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+  {
+    title: 'Boneless Croaker Noodle with Pickled Cabbage 雪菜黄鱼面',
+    price: '149kr',
+    tags: ' Recommend | 👍👍👍👍👍',
+  },
+
+  {
+    title: 'Soy sauce braised pork chop 红烧大排',
+    price: '129 kr',
+    tags: ' Recommend | 👍👍👍👍',
+  }, 
+
+  {
+    title: 'Braised Pork Belly 红烧肉',
+    price: '129 kr',
+    tags: ' Recommend | 👍👍👍👍',
+  },
+
+  {
+    title: 'Sweet and Sour Ribs 糖醋排骨 ',
+    price: '149 kr',
+    tags: ' Recommend | 👍👍👍👍 ',
+  },
+  {
+    title: 'Meat Stuffed Gluten Balls 油面筋塞肉 ',
+    price: '119 kr',
+    tags: ' Recommend | 👍👍👍👍',
+  },
+  {
+    title: 'bamboo shoot soup with fresh and pickled streaky pork 腌笃鲜 ',
+    price: '199 kr',
+    tags: ' Recommend | 👍👍👍👍',
+  },
+]
+
+const beer_drinks = [
+  
+  {
+    title: 'Tsingtao premium lager',
+    price: '55 kr /330ml',
+    tags: ' ABV:4.3%',
+  },
+
+  {
+    title: 'Tsingtao pure draft',
+    price: '58 kr /330ml',
+    tags: ' ABV:4.3%',
+  },
+
+  {
+    title: 'Tsingtao wheat beer',
+    price: '68 kr /330ml',
+    tags: ' ABV:4.7% ',
+  },
+
+  {
+    title: 'Tsingtao Stout',
+    price: '78 kr /330ml',
+    tags: ' ABV:4.8%',
+  },
+
+
+  {
+    title: 'Tsingtao IPA',
+    price: '78 kr /330ml',
+    tags: ' ABV:6.2%',
+  },
+
+  {
+    title: 'Coca cola',
+    price: '33 kr',
+    tags: ' 330 ml',
+  },
+
+  {
+    title: 'Sprite',
+    price: '33 kr',
+    tags: ' 330 ml',
+  },
+  {
+    title: 'Fanta',
+    price: '33 kr',
+    tags: ' 330 ml',
+  },
+ 
+];
+
+export default {cold_menu, menu, beer_drinks };
