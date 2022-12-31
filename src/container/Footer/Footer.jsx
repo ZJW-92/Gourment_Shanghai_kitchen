6bfde9d4bb0f3ca0aabd86132271d1e6aafececc
ZJW-92 <zjwei0201@gmail.com>
2022-12-31 14:14:28 +0100

    add footer in container

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+  <div className="app__bg app__footer section__padding" id="login">
+
+    <div className="footer__copyright">
+      <p className="p__cormorant">2023 Shanghai Kitchen. All Rights Reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
